a3290b333286e3352fb889f1fb0be9eb88399064
ljg521 <542553031@qq.com>
2018-05-08 14:47:33 +0800

    first commit

--- laravel/resources/views/form.php
+++ laravel/resources/views/form.php
@@ -2,5 +2,6 @@
 <form action="/req/add" method="post">
     <input name="input">
     <button type="submit">提交</button>
+    <button type="submit">提交2</button>
     <input name="_token" type="hidden" value="{{csrf_token()}}">
 </form>
